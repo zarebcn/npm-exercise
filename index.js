61ed88dc225872e36a2d931972cbc54924b07ffc
zare <elzarebcn@gmail.com>
2017-10-22 18:35:01 +0200

    did a simple sample of gulp sass and autoprefixer

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 
 const Person = require('./person.js');
+const axios = require('axios');
 
 let p1 = new Person('Peter', 30);
 
 console.log(p1.name);
+
+axios
+  .get('https://pokeapi.co/api/v2/pokemon/')
+  .then(function (response) {
+    let datos = response.data.results;
+    let i = 0;
+    while (i < datos.length) {
+      console.log(datos[i].name);
+      i++;
+    }
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
